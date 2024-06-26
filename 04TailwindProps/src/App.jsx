d0fb5e42f30f3fb3339576fdfe4ea934d644cd3b
Cleansword <sagarjar213@gmail.com>
2024-06-26 17:50:11 +0530

    using props with tailwind css

---
+++ 04TailwindProps/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Card from './component/Card'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-blue-400 mb-4'>Tailwind Text</h1>
+      <Card username="sagar" addtext="visit me" />
+      <Card />
+      
+
+    </>
+  )
+}
+
+export default App
